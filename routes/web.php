382149274a763cde47cfee3547ca43290a9bfc0a
kim320 <hj_kim0320@likelion.org>
2020-02-27 23:51:53 +0900

    라우팅

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::pattern('foo', '[0-9a-zA-Z]{5}');
+// Route::get('/{foo?}', function ($foo = 'abcde') {
+//     return $foo;
+// });
+
+Route::get('/',[
+    'as' => 'home',
+    function () {
+        return '제 이름은 "home" 입니다.';
+    }
+]);
+
+Route::get('/home', function () {
+    return redirect(route('home'));
 });
