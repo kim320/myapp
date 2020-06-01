3d13310bfbb2c6d4a2ea8d59ecabf2a3f527f575
kim320 <hj_kim0320@likelion.org>
2020-06-02 00:01:43 +0900

    사용자 인증

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,39 @@
 */
 
 Route::get('/', 'WelcomeController@index');
-Route::resource('articles', 'ArticlesController');
+
+Route::get('auth/login', function () {
+    $credentials = [
+        'email' => 'khj@example.com',
+        'password' => 'password'
+    ];
+
+    if(!auth()->attempt($credentials)) {
+        return '로그인 정보가 정확하지 않습니다.';
+    }
+    
+    return redirect('protected');
+});
+
+// Route::get('protected', function () {
+//     dump(session()->all());
+
+//     if(!auth()->check()){
+//         return '누구세요?';
+//     }
+
+//     return '어서오세요'. auth()->user()->name;
+// });
+
+Route::get('protected', ['middleware' => 'auth', function() {
+    return '어서오세요'. auth()->user()->name;
+}]);
+
+Route::get('auth/logout', function () {
+    auth()->logout();
+
+    return '또 봐요!';
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
